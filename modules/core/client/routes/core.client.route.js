5879720eb713df186af370c0162a9f66ac9fcb12
KarthikChilukamarri <karthik.chilukamarri@gmail.com>
2016-08-16 16:03:05 -0700

    App_Complete!

--- modules/core/client/routes/core.client.route.js
+++ modules/core/client/routes/core.client.route.js
@@ -25,6 +25,7 @@ angular
                             return $stateParams.contactId
                         }
                     },
+
                     controller:'editCtrl'
 
                 });
